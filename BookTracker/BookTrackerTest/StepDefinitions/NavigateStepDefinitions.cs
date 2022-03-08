dc875ffc52eb479f45e4d680650248913499029d
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2022-03-08 11:17:47 -0500

    Added missing step definitions

---
+++ BookTracker/BookTrackerTest/StepDefinitions/NavigateStepDefinitions.cs
@@ -0,0 +1,26 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace BookTrackerTest.StepDefinitions
+{
+    [Binding]
+    public class NavigateStepDefinitions
+    {
+        IWebDriver webDriver = new ChromeDriver();
+        string visit = "";
+        [Given(@"the link to go to")]
+        public void GivenTheLinkToGoTo()
+        {
+            visit = "http://google.com";
+        }
+
+        [Then(@"Navigate to the page")]
+        public void ThenNavigateToThePage()
+        {
+            webDriver.Navigate().GoToUrl(visit);
+            webDriver.Dispose();
+        }
+    }
+}
